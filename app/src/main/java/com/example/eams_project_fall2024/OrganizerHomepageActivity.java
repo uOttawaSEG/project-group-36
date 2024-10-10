cb394bcdf96c9bf0fda147b2011743733962c823
Anima Mehraj Mehrin <animamm2003@gmail.com>
2024-10-10 01:54:59 -0400

    updated the homepage for organiser

--- app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
+++ app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
@@ -1,5 +1,25 @@
 package com.example.eams_project_fall2024;
 
-public class OrganizerHomepageActivity {
+import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OrganizerHomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_organiser_homepage); // Referencing your XML layout
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearLayout2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
