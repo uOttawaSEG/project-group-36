8b77bd9c49a12e3753494ce9de55b849537f7233
James <Jatti045@uottawa.ca>
2024-10-14 19:48:21 -0400

    Implemented simple boilerplate for login functionality

--- app/src/main/java/com/example/eams_project_fall2024/LoginActivity.java
+++ app/src/main/java/com/example/eams_project_fall2024/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.eams_project_fall2024;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,8 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    //Administrator admin = new Administrator();
     private TextView signupLink;
+    private EditText loginEmail, loginPassword;
+    private FirebaseAuth auth;
+    private Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +31,56 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        signupLink = findViewById(R.id.signupLink);
+
         SharedPreferences preferences = getSharedPreferences("YourAppPreferences", MODE_PRIVATE);
         String userEmail = preferences.getString("UserEmail", null);
 
-        signupLink = findViewById(R.id.signupLink);
 
         signupLink.setOnClickListener(view -> {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
         });
+
+        loginButton.setOnClickListener(view -> {
+            loginUser();
+        });
+
+    }
+
+    private void loginUser() {
+        String email = loginEmail.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            loginEmail.setError("Email is required");
+            loginPassword.requestFocus();
+            return;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            loginEmail.setError("Please enter a valid email");
+            loginEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            loginPassword.setError("Password is required");
+            loginPassword.requestFocus();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(LoginActivity.this, AttendeeHomepageActivity.class));
+                    } else {
+                        loginPassword.setError("Login failed. Check your credentials");
+                        loginEmail.requestFocus();
+                        loginPassword.requestFocus();
+                    }
+                });
     }
 
     // need to add implementation of storing user's email after successful login!
