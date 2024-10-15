b708f0a1731a52fbd7623fad4bcf6b7215396666
Anima Mehraj Mehrin <animamm2003@gmail.com>
2024-10-15 17:20:16 -0400

    resolved the organizer sign up

--- app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
+++ app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class OrganizerHomepageActivity extends AppCompatActivity {
@@ -12,7 +18,13 @@ public class OrganizerHomepageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_organiser_homepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearLayout2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 
     public void logout(View view) {
