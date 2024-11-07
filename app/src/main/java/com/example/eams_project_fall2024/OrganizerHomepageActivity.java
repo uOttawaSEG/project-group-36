4ccf8e4d287a6dfe455f2fd0019d15adab93841d
James <Jatti045@uottawa.ca>
2024-11-07 17:13:09 -0500

    Implemented mapping between createEvent button and createEventActivity.

--- app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
+++ app/src/main/java/com/example/eams_project_fall2024/OrganizerHomepageActivity.java
@@ -4,20 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import javax.microedition.khronos.egl.EGLDisplay;
 
 public class OrganizerHomepageActivity extends AppCompatActivity {
 
+    private Button createEventButton;
     private Button viewUpcomingEventsButton;
     private Button viewPastEventsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_organiser_homepage);
 
+        createEventButton = findViewById(R.id.createEventButton);
         viewUpcomingEventsButton = findViewById(R.id.viewUpcomingEventsButton);
         viewPastEventsButton = findViewById(R.id.viewPastEventsButton);
+
+        createEventButton.setOnClickListener(view -> {
+            startActivity(new Intent(OrganizerHomepageActivity.this, CreateEventActivity.class));
+        });
 
         // Set OnClickListener for View Upcoming Events button
         viewUpcomingEventsButton.setOnClickListener(new View.OnClickListener() {
